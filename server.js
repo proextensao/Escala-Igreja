ae7974d1bedbbdad83cbb4b93959d3ab5ba2f64a
Israel de Sá <raelmaciel79@gmail.com>
2026-06-12 11:27:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: "*", 
   methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "api-key"]
 }));
 app.use(express.json());
 
